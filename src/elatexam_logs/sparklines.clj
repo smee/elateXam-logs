b4f22f7495136532589ff8e9638da56dffa74ebb
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-09-10 17:16:47 +0200

    [internal] indentation

--- src/elatexam_logs/sparklines.clj
+++ src/elatexam_logs/sparklines.clj
@@ -23,30 +23,29 @@
 (defn make-sparkline 
   "Create pixel-accurate sparkline bitmap graphic from {:width :height :values :marker}. :marker=-1 for last entry."
    [{w :width, h :height, v :values, m :marker, m-w :marker-width, :or {w 100, h 30, m -1, m-w 2}}]
-  (println m-w)
    (let [v       (scale-values v h)
-        bitmap  (make-buffered-image w h)
-        g       (. bitmap (getGraphics))
-        m       (if (and m (< m 0)) (+ m (count v)) m)
-        step    (/ (- w 3) (- (count v) 1))]
-    (do (doto g (.setColor (java.awt.Color/white))
-                (.fillRect 0 0 w h)
-                (.setColor (java.awt.Color/gray)))
-        (dotimes  [idx  (dec (count v))]
-          (let [x-pos    (inc (* idx step))
-                prev-val (nth v idx)
-                this-val (nth v (inc idx))]
-            (when (and prev-val this-val)
-                  (. g (drawLine x-pos (dec prev-val) (+ x-pos step) (dec this-val))))))
-        ; Draw marker if present and return the sparkline bitmap
-        (when (and m (nth v m))
-            (let [bx  (* m step)
-                  by  (- (nth v m) 2)]
-              (doto g (.setColor (java.awt.Color/red))
-                      (.fillOval bx by m-w m-w))))
-        (. g (dispose))
-        bitmap)))
- 
+         bitmap  (make-buffered-image w h)
+         g       (. bitmap (getGraphics))
+         m       (if (and m (< m 0)) (+ m (count v)) m)
+         step    (/ (- w 3) (- (count v) 1))]
+     (doto g (.setColor (java.awt.Color/white))
+       (.fillRect 0 0 w h)
+       (.setColor (java.awt.Color/gray)))
+     (dotimes  [idx  (dec (count v))]
+       (let [x-pos    (inc (* idx step))
+             prev-val (nth v idx)
+             this-val (nth v (inc idx))]
+         (when (and prev-val this-val)
+           (. g (drawLine x-pos (dec prev-val) (+ x-pos step) (dec this-val))))))
+     ; Draw marker if present and return the sparkline bitmap
+     (when (and m (nth v m))
+       (let [bx  (* m step)
+             by  (- (nth v m) 2)]
+         (doto g (.setColor (java.awt.Color/red))
+           (.fillOval bx by m-w m-w))))
+     (. g (dispose))
+     bitmap))
+
 (defn show-sparkline
   [values]
   (let [spark (make-sparkline {:width 300 
